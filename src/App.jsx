9eb79e47af67a5234acd896e4fc4f512c52bb01f
Lilas Alnesr <lelas.alneser@gmail.com>
2026-06-28 18:26:59 +0200

    fix app.jsx and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import AdminLayout from './components/AdminLayout'
 import UserLayout from './components/UserLayout'
 import GetAlphaBit from './components/GetAlphaBit'
-import './App.css'  
+import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    // LÄGG TILL BASENAME HÄR:
+    <BrowserRouter basename="/task7-port">
       <nav className="navbar">
         <div className="navbar-brand">
           <span>MyPortfolio</span>
         </div>
-        
+
         <div className="navbar-links">
           <Link to="/admin">Admin</Link>
           <Link to="/user">User</Link>
